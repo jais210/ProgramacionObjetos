6f1af2039c40f5a49551db3eececb7f18b0c9086
jais210 <jakeline_210@hotmail.com>
2017-07-25 11:47:03 -0500

    nuevo

---
+++ assets/js/index.js
@@ -0,0 +1,98 @@
+function Pasajeros(nombres,apellidos,dni){
+    this.nombres = nombres;
+    this.apellidos = apellidos;
+    this.dni = dni;
+
+    var html = ""; 
+      html += this.nombres + " - ";
+      html += this.apellidos + " -> ";
+      html += this.dni;
+      return html;
+}
+
+
+function Bus(){
+    this pasajeros = [];
+    this.asientos=document.getElementById("td");
+
+}
+
+
+var numeroAsiento = -1;
+var asientos = document.getElementsByTagName("td");
+
+var pasajeros = new Array(32);
+for (var i = 0; i < 32; i++) {
+  pasajeros[i] = undefined;
+}
+
+for (var i = 0; i < asientos.length; i++) {
+  asientos[i].addEventListener("click", redirect, false);
+}
+
+function redirect(event) {
+  numeroAsiento = event.target.textContent;
+  event.target.style.backgroundColor =
+    event.target.style.backgroundColor == "rgb(248, 237, 80)"
+      ? "transparent"
+      : "#F8ED50";
+}
+
+function buscar() {
+  var ingreso = document.getElementById("dnibuscar").value;
+  for (var i = 0; i < 32; i++) {
+    if (pasajeros[i] != undefined && pasajeros[i].dni == ingreso) {
+      document.getElementById("nombre").value = pasajeros[i].nombre;
+      document.getElementById("apellido").value = pasajeros[i].apellido;
+      document.getElementById("dni").value = pasajeros[i].dni;
+    }
+  }
+}
+
+function reservar() {
+  var nombre = document.getElementById("nombre").value;
+  var apellido = document.getElementById("apellido").value;
+  var dni = document.getElementById("dni").value;
+
+  pasajeros[numeroAsiento - 1] = {
+    nombre: nombre,
+    apellido: apellido,
+    dni: dni
+  };
+  alert("registrado");
+  limpiar();
+  console.log(pasajeros);
+}
+
+function limpiar() {
+  document.getElementById("nombre").value = "";
+  document.getElementById("apellido").value = "";
+  document.getElementById("dni").value = "";
+}
+
+function mostrarLista() {
+  var lista = [];
+
+  for (var i = 0; i < 32; i++) {
+    if (pasajeros[i] != undefined) {
+      var mostrarLista = document.getElementById("mostrarLista");
+      lista +=
+        "<br> El pasajero es " +
+        pasajeros[i].nombre +
+        " " +
+        pasajeros[i].apellido +
+        ", " +
+        "<br> su número de DNI es " +
+        pasajeros[i].dni + "<br>";
+
+      mostrarLista.innerHTML = lista;
+    }
+  }
+}
+
+function cancelar() {
+  pasajeros[numeroAsiento - 1] = undefined;
+  alert("asiento cancelado")
+  numeroAsiento = event.target.textContent;
+  event.target.style.backgroundColor = "transparent";
+}
